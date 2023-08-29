28c82b4118735f72a0d54380c1042cd9e67a1f77
adilmukhtar-tugraz <63244329+adilmukhtar-tugraz@users.noreply.github.com>
2023-08-29 10:45:02 +0200

    model deployment

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
 from model_handler import ModelVehicleDamageClassification
+import random
 
 app = FastAPI()
-model_vehicle_classification = ModelVehicleDamageClassification()
+obj_model_veh_clfxn = ModelVehicleDamageClassification()
 
 
 @app.get('/')
 async def home():
-    print(model_vehicle_classification.getSTunedModel())
+    print(obj_model_veh_clfxn.getTunedModel())
     return {"AVL": "This is the home page."}
 
 @app.get('/classify')
 async def classify():
+    vehicle_instance = obj_model_veh_clfxn.getRandomVehicleInstance()
+    X = vehicle_instance.drop(['LABEL'], axis=1).values
+    y = vehicle_instance['LABEL']
+    X_preprocessed = obj_model_veh_clfxn.preProcessInstance(X)
+    classification = obj_model_veh_clfxn.classify(X_preprocessed)
+    print(classification)
+    #print(classification)
     return {"damage": 0}
 
--- app/model_handler.py
+++ app/model_handler.py
@@ -1,13 +1,15 @@
 import pickle
+import pandas as pd
+import random
 
 class ModelVehicleDamageClassification:
 
     # load models for classification in the constructor
     def __init__(self):
-        self.scaler_model = self.loadModels('../model/scaler.sav')
-        self.tuned_model = self.loadModels('../model/tuned_rf_model.sav')
-        self.log_model = self.loadModels('../model/log_model.sav')
-
+        self.scaler_model = self.loadModels('../models/scaler.sav')
+        self.tuned_model = self.loadModels('../models/tuned_rf_model.sav')
+        self.log_model = self.loadModels('../models/log_model.sav')
+        self.df_test_data = self.loadTestData('../data/test_logged_transformed_data.csv')
 
     # load specific model
     def loadModels(self,path):
@@ -16,11 +18,39 @@ class ModelVehicleDamageClassification:
         except Exception as e:
             print(e)
 
+    # load test data for predicitons
+    def loadTestData(self, path):
+        try:
+            df_test = pd.read_csv(path)
+            df_test = df_test.loc[:, ~df_test.columns.str.contains('^Unnamed')]
+            return df_test
+        
+        except Exception as e:
+            print(e)
+    
+    def preProcessInstance(self, X):
+        return self.getScalerModel().transform(X)
+
+    def classify(self, X):
+       return self.getTunedModel().predict(X)[0]
+       
+    
+    # randomly select vehicle data from test data for prediction
+    def getRandomVehicleInstance(self):
+        return self.getTestData().sample()
+
+    # return test data
+    def getTestData(self):
+        return self.df_test_data
+    
+    # return saved scaler model
     def getScalerModel(self):
         return self.scaler_model
     
+    # return saved classification model
     def getTunedModel(self):
         return self.tuned_model
     
+    # return saved log model
     def getLogModel(self):
         return self.log_model
